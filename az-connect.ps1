46aef364d7f1f54d8f566a950b7c350c000fd02b
Robert Crane <director@ciaops.com>
2018-07-01 22:59:43 +1000

    Multi subscriptions

--- az-connect.ps1
+++ az-connect.ps1
@@ -4,18 +4,27 @@
 ## Prerequisites = 1
 ## 1. Ensure azurerm module installed or updated
 
-## ensure that install-module msonline has been run
-## ensure that update-module msonline has been run to get latest module
+## ensure that install-azurerm msonline has been run
+## ensure that update-azurerm msonline has been run to get latest module
+## Module = https://www.powershellgallery.com/packages/AzureRM/
+## Latest version 6.3.0 June 2018
 Import-module azurerm
 
 Login-azurermaccount
 
 ## Select ARM account
 $subscription=get-azurermsubscription
-## determine if there multiple subscriptions
-## if not then select just that one
-## if so select the first one
 
-Set-AzureRmContext -SubscriptionID $subscription.Id[0]  ## select first subscription available
+if ($subscription.count -lt 2){         ## See if there are multiple subscriptions in tenant
+    ## Only one subscription in tenant so set this as default
+    Set-AzureRmContext -SubscriptionID $subscription    
+}
+else {
+    # More than one subscription, set the first one as current
+    Set-AzureRmContext -SubscriptionID $subscription.Id[0]
+    ## Select-AzureSubscription  -SubscriptionID $subscription.Id[0] –Default     
+}
+
+## Get-AzureSubscription -SubscriptionDataFile "C:\Temp\MySubscriptions.xml
 
 write-host -foregroundcolor green "Now connected to Azure"
