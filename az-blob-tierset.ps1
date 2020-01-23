dddfad1da6b46f323d9366f4ba85d64d9a1d0f8f
Robert Crane <director@ciaops.com>
2020-01-24 09:01:08 +1100

    Source

---
+++ az-blob-tierset.ps1
@@ -0,0 +1,53 @@
+<# CIAOPS
+Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+Description - Change the tier l;evel of items inside an Azure storage account. The storage account will need to support those levels.
+Source - 
+
+Source - https://github.com/directorcia/Office365/blob/master/graph-connect.ps1
+
+Prerequisites = 2
+1. Already connected to Azure - https://github.com/directorcia/Azure/blob/master/az-connect.ps1
+2. Change the variable to match Storage Account name, Resource Group and desired tier
+
+More scripts available by joining http://www.ciaopspatron.com
+
+#>
+
+## Variables
+$systemmessagecolor = "cyan"
+$processmessagecolor = "green"
+$storageaccountname = "<your storage account name here>"
+$storageresourcegroup = "<your storage account resource group name here>"
+$storagetier = "<your desired storage tier level here>"     # Hot, Cool or Archive
+
+clear-host
+
+write-host -foregroundcolor $systemmessagecolor "Script started`n"
+
+write-host -foregroundcolor $processmessagecolor "Get Storage Account"
+$storageaccount = Get-AzStorageAccount -name $storageaccountname -ResourceGroupName $storageresourcegroup
+
+write-host -foregroundcolor $processmessagecolor "Get Storage Account key"
+$key = (get-azstorageaccountkey -ResourceGroupName $storageaccount.ResourceGroupName -Name $storageaccount.StorageAccountName).value[0]
+
+write-host -foregroundcolor $processmessagecolor "Get Storage context"
+$context = New-AzstorageContext -StorageAccountName $storageaccount.StorageAccountName -StorageAccountKey $key
+
+write-host -foregroundcolor $processmessagecolor "Get Storage containers"
+$storagecontainers = get-azstoragecontainer -Context $context
+
+write-host -foregroundcolor $processmessagecolor "Get Items"
+$Blobs = @()
+foreach ($StorageContainer in $StorageContainers) {
+    $Blobs += Get-AzStorageBlob -Context $Context -Container $StorageContainer.Name
+}
+
+write-host -foregroundcolor $processmessagecolor "Change tier of all items`n"
+Foreach ($Blob in $Blobs) {
+    #Set tier of all blobs to desired tier
+    $blob.icloudblob.SetStandardBlobTier($StorageTier)
+    write-host  "Changed -", $blob.name
+}
+
+write-host -foregroundcolor $systemmessagecolor "Script finished`n"
--- az-connect.ps1
+++ az-connect.ps1
@@ -1,8 +1,8 @@
 <# CIAOPS
 Script provided as is. Use at own risk. No guarantees or warranty provided.
 
-Description
-Script designed to login to Azure resources
+Description - Script designed to login to Azure resources
+Source - https://github.com/directorcia/Azure/blob/master/az-connect.ps1
 
 Prerequisites = 1
 1. Ensure azurerm module installed or updated
@@ -24,6 +24,7 @@ Clear-Host
 
 write-host -foregroundcolor $systemmessagecolor "Script started`n"
 
+write-host -foregroundcolor $processmessagecolor "Azure PowerShell module loading...."
 Import-module az
 write-host -foregroundcolor $processmessagecolor "Azure PowerShell module loaded"
 
@@ -31,15 +32,16 @@ connect-azaccount
 
 ## Select ARM account
 
-$subscription=get-azsubscription
+$subscription = get-azsubscription
 
-if ($subscription.count -lt 2){         ## See if there are multiple subscriptions in tenant
+if ($subscription.count -lt 2) {
+    ## See if there are multiple subscriptions in tenant
     ## Only one subscription in tenant so set this as default
     Set-AzContext -SubscriptionID $subscription    
 }
 else {
     # More than one subscription, set the first one as current
-    write-host -foregroundcolor $processmessagecolor $subscription.count,"subscriptions detected."
+    write-host -foregroundcolor $processmessagecolor $subscription.count, "subscriptions detected."
     write-host -foregroundcolor $warnmessagecolor "Selecting subscription = ", $subscription.name[0]
     Set-AzContext -SubscriptionID $subscription.Id[0]
     ## Select-AzureSubscription  -SubscriptionID $subscription.Id[0] –Default     
