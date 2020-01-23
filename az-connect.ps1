0174751a7c13878ea1aece14c874fe0fd8680266
Robert Crane <director@ciaops.com>
2020-01-23 20:32:54 +1100

    Update to AZ

--- az-connect.ps1
+++ az-connect.ps1
@@ -7,10 +7,8 @@ Script designed to login to Azure resources
 Prerequisites = 1
 1. Ensure azurerm module installed or updated
 
-ensure that install-azurerm msonline has been run
-ensure that update-azurerm msonline has been run to get latest module
-Module = https://www.powershellgallery.com/packages/AzureRM/
-Latest version 6.13.1 21 November 2018
+ensure that install-az msonline has been run
+ensure that update-az msonline has been run to get latest module
 
 Allow custom scripts to run just for this instance
 set-executionpolicy -executionpolicy bypass -scope currentuser -force
@@ -26,24 +24,24 @@ Clear-Host
 
 write-host -foregroundcolor $systemmessagecolor "Script started`n"
 
-Import-module azurerm
+Import-module az
 write-host -foregroundcolor $processmessagecolor "Azure PowerShell module loaded"
 
-Login-azurermaccount
+connect-azaccount
 
 ## Select ARM account
 
-$subscription=get-azurermsubscription
+$subscription=get-azsubscription
 
 if ($subscription.count -lt 2){         ## See if there are multiple subscriptions in tenant
     ## Only one subscription in tenant so set this as default
-    Set-AzureRmContext -SubscriptionID $subscription    
+    Set-AzContext -SubscriptionID $subscription    
 }
 else {
     # More than one subscription, set the first one as current
     write-host -foregroundcolor $processmessagecolor $subscription.count,"subscriptions detected."
     write-host -foregroundcolor $warnmessagecolor "Selecting subscription = ", $subscription.name[0]
-    Set-AzureRmContext -SubscriptionID $subscription.Id[0]
+    Set-AzContext -SubscriptionID $subscription.Id[0]
     ## Select-AzureSubscription  -SubscriptionID $subscription.Id[0] –Default     
 }
 
