9f527b21cd322db9bd6a5ded0ec63bff7168787d
Robert Crane <director@ciaops.com>
2025-01-26 11:20:35 +1100

    New lesson

---
+++ Iot/Keyestudio KS0172/Lesson_1.2/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+/*
+  keyestudio sun_follower
+  lesson 1.1
+  Blink
+  http://www.keyestudio.com
+*/
+#define LED  3 //define the pin of LED as D3
+void setup()
+{
+  pinMode(LED, OUTPUT);// initialize digital pin LED as an output.
+}
+void loop() // the loop function runs over and over again forever
+{
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level
+  delay(100);    // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);    // wait for a second
+}
